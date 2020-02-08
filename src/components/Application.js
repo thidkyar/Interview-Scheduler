63d862e1dec7951454184f5e6dc574331bd72cae
Taranvir Hidkyar <thidkyar@gmail.com>
2020-02-08 14:40:36 -0500

    finish project setup and understand different development servers

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,14 +3,26 @@ import React from "react";
 import "components/Application.scss";
 
 export default function Application(props) {
-  return (
-    <main className="layout">
-      <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
-      </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
-    </main>
-  );
+    return (
+        <main className="layout">
+            <section className="sidebar">
+                <img
+                    className="sidebar--centered"
+                    src="images/logo.png"
+                    alt="Interview Scheduler"
+                />
+                <hr className="sidebar__separator sidebar--centered" />
+                <nav className="sidebar__menu"></nav>
+                <img
+                    className="sidebar__lhl sidebar--centered"
+                    src="images/calendar.png"
+                    alt="Calendar"
+                    style={{maxWidth: "48px", maxHeight: "48px"}}
+                />
+            </section>
+            <section className="schedule">
+                {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+            </section>
+        </main>
+    );
 }
